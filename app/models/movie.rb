9c616e1a08559d286e862993455135c441bd29e4
github username <github email address>
2019-07-30 10:40:15 -0700

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,21 @@ class Movie
     @@all
   end
 
+  def queue_items
+    QueueItem.all.select {|item| item.movie == self}
+  end
+
+  def viewers
+    queue_items.map {|item| item.viewer} 
+  end
+
+  def average_rating
+    queue_items.map {|item| item.rating}.sum/length
+  end
+
+  def self.highest_rated
+    QueueItem.all.select {|item| item.rating}.max
+  end
+
+
 end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,21 @@
 class QueueItem
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating = nil)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
 
 
 end
