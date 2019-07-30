84bc522cce8e30ba807c550a46298cfea817f2a2
ecargiohc <therealgracechoi@gmail.com>
2019-07-30 10:34:12 -0700

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,5 +11,41 @@ class Movie
   def self.all
     @@all
   end
+#returns an array of all the `QueueItem` 
+#instances that contain this movie
+  def queue_items
+    QueueItem.all.select do |q|
+      q.movie == self
+    end
+  end
+#eturns an array of all of the `Viewer`s with this 
+#`Movie` instance in their queue
+
+  def viewers
+    Viewer.all.select do |v|
+      v.movie == self
+    end
+  end
+#returns the average of all ratings 
+#for this instance of `Movie`
+  def average_rating
+  # avg = total movies/movie
+    total_ratings = QueueItem.all.select do |m|
+      m.rating == self
+    end
+    total_movies = self.all.length
+    avg = total_movies.to_f / total_ratings.to_f
+    avg
+  end
+
+#returns the instance of `Movie` 
+#with the highest average rating
+  def self.highest_rated
+    self.average_rating.max_by {|r|
+      r.rating}
+  end
+
+
 
 end
+
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,16 @@
 class QueueItem
 
+    attr_accessor :movie, :viewer, :rating
+    @@all = []
 
+    def intialize(movie, viewer, rating=nil)
+        @movie = movie
+        @viewer = viewer
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
